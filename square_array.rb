396d0f9dc9563e499d1cfe3d4f14cc9a5bb7a6e8
Santiago Sanchez <chagostwo@yahoo.com>
2019-05-16 04:14:10 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
-  #new_array = []
-  #array.each{|num|new_array.push(num ** 2)}
-  #new_array
-  array.collect{|num|num**2}
+  new_array = []
+  array.each{|num|new_array.push(num ** 2)}
+  new_array
+
 end
